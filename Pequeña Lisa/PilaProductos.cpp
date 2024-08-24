e473a31d79a7fdd943984cc1e29474ea5f65b9f0
AndresgeAlfaro <andresgealfaro04@gmail.com>
2024-08-23 18:21:10 -0600

    metodos vacios PilaProductos .h y .cpp

--- Pequeña Lisa/PilaProductos.cpp
+++ Pequeña Lisa/PilaProductos.cpp
@@ -1,1 +1,20 @@
 #include "PilaProductos.h"
+
+void PilaProductos::pushProducto(Producto)
+{
+}
+
+Producto PilaProductos::popProducto()
+{
+	return Producto();
+}
+
+Producto PilaProductos::serchProductoID(int)
+{
+	return Producto();
+}
+
+int PilaProductos::serchProductoporTipo()
+{
+	return 0;
+}
--- Pequeña Lisa/PilaProductos.h
+++ Pequeña Lisa/PilaProductos.h
@@ -1,5 +1,23 @@
-#pragma once
+#ifndef PilaProductos
+#include "Producto.h"
+
+struct Nodo {
+
+};
+
 class PilaProductos
 {
+public:
+
+	void pushProducto(Producto);
+	Producto popProducto();
+	Producto serchProductoID(int);
+	int serchProductoporTipo();
+
+private:
+	std::string producto;
+
 };
+
+#endif // !PilaProductos
 
