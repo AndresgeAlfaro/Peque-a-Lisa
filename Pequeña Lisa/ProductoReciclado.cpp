2ec274db59aea398ac2aed59177f0e0c88865a66
AndresgeAlfaro <andresgealfaro04@gmail.com>
2024-08-27 09:25:55 -0600

    ProductoReciclado Terminado

--- Pequeña Lisa/ProductoReciclado.cpp
+++ Pequeña Lisa/ProductoReciclado.cpp
@@ -1,1 +1,26 @@
 #include "ProductoReciclado.h"
+
+ProductoReciclado::ProductoReciclado(Date fechaReciclaje, std::string descripcionR): fechaReciclaje(fechaReciclaje), descripcionR(descripcionR){}
+
+Date ProductoReciclado::getFechaReciclaje()
+{
+	return fechaReciclaje;
+}
+
+std::string ProductoReciclado::getDescripcionR()
+{
+	return std::string();
+}
+
+void ProductoReciclado::setFechaReciclaje(Date fechaReciclaje)
+{
+	this->fechaReciclaje = fechaReciclaje;
+}
+
+void ProductoReciclado::setDescripcionR(std::string descripcionR)
+{
+	this->descripcionR = descripcionR;
+}
+
+
+
--- Pequeña Lisa/ProductoReciclado.h
+++ Pequeña Lisa/ProductoReciclado.h
@@ -1,16 +1,28 @@
 #ifndef PRODUCTORECILADO_H
 #define	PRODUCTORECILADO_H
 #include "Producto.h"
+#include "Date.h"
 
-class ProductoReciclado:public Producto
+class ProductoReciclado
 {
+
 public:
 
+	ProductoReciclado(Date, std::string);
+	~ProductoReciclado();
 
+	Date getFechaReciclaje();
+	std::string getDescripcionR();
+
+	void setFechaReciclaje(Date);
+	void setDescripcionR(std::string);
+
+	
 
 private:
 
-	
+	Date fechaReciclaje;
+	std::string descripcionR;
 
 };
 
